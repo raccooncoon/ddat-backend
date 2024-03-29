ec064b009aafd3af5a1db5847a1d9fd846c3c63c
임민영 <raccoon@shinsegae.com>
2024-03-29 10:20:01 +0900

    Revert "모듈삭제"
    This reverts commit 8843920e2b5cadda6740bfec2641e7d385969392.

--- src/main/java/com/raccoon/ddat/repository/XmlFileRepository.java
+++ src/main/java/com/raccoon/ddat/repository/XmlFileRepository.java
@@ -4,17 +4,11 @@ import com.raccoon.ddat.entity.XmlFileEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface XmlFileRepository extends JpaRepository<XmlFileEntity, Long> {
     Page<XmlFileEntity> findByContextContainsAndSubtagInOrderByUrlCountDesc(String search, List<String> subtags, Pageable pageable);
-
     Page<XmlFileEntity> findBySubtagInOrderByUrlCountDesc(List<String> subtags, Pageable pageable);
-
-    void deleteByModuleName(String moduleName);
-
-    @Query("SELECT x.moduleName, COUNT(x) FROM XmlFileEntity x GROUP BY x.moduleName")
-    List<Object[]> countByModuleName();
+    List<XmlFileEntity> findAll();
 }
